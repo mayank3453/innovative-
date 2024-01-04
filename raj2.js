d9e2b803d78b0de7c8ed3b401090e1f73934656a
mayank3453 <155183015+mayank3453@users.noreply.github.com>
2024-01-04 15:20:03 +0530

    raj2.js

---
+++ raj2.js
@@ -0,0 +1,461 @@
+const url1 = 'https://api.thingspeak.com/channels/2388995/feeds.json?results=15';
+const option = {
+	method: 'GET',
+	headers: {
+		'Y-RapidAPI-Key': '0NUMAZAF2B3746T0',
+		'Y-RapidAPI-Host': 'api.thingspeak.com'
+	}
+};
+
+fetch(' https://api.thingspeak.com/channels/2388995/feeds.json?results=15', Option)
+.then(response => response.json())
+.then((response) => {
+
+console.log(response)
+
+
+
+
+
+if(response.feeds[0].field1  !=null){tempir.innerHTML=response.feeds[0].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[1].field1  !=null){tempir.innerHTML=response.feeds[1].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+
+if(response.feeds[2].field1 !=null){tempir.innerHTML=response.feeds[2].field1}
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+if(response.feeds[3].field1  !=null){tempir.innerHTML=response.feeds[3].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+if(response.feeds[4].field1  !=null){tempir.innerHTML=response.feeds[4].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[5].field1 !=null){tempir.innerHTML=response.feeds[5].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[6].field1  !=null){tempir.innerHTML=response.feeds[6].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[7].field1  !=null){tempir.innerHTML=response.feeds[7].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[8].field1  !=null){tempir.innerHTML=response.feeds[8].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[9].field1  !=null){tempir.innerHTML=response.feeds[9].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+if(response.feeds[10].field1 !=null){tempir.innerHTML=response.feeds[10].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[11].field1  !=null){tempir.innerHTML=response.feeds[11].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[12].field1  !=null){tempir.innerHTML=response.feeds[12].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[13].field1  !=null){tempir.innerHTML=response.feeds[13].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+if(response.feeds[14].field1  !=null){tempir.innerHTML=response.feeds[14].field1 }
+else{tytyty.innerHTML=tytyty.innerHTML}
+
+
+
+
+
+
+    
+    if(response.feeds[0].field2  !=null){humii.innerHTML=response.feeds[0].field2 }
+    else{humii22.innerHTML=humii22.innerHTML}
+ 
+
+   if(response.feeds[1].field2  !=null){humii.innerHTML=response.feeds[1].field2 }
+   else{humii22.innerHTML=humii22.innerHTML}
+
+
+
+    if(response.feeds[2].field2 !=null){humii.innerHTML=response.feeds[2].field2}
+    else{humii22.innerHTML=humii22.innerHTML}
+
+  if(response.feeds[3].field2  !=null){humii.innerHTML=response.feeds[3].field2 }
+ else{humii22.innerHTML=humii22.innerHTML}
+
+    if(response.feeds[4].field2  !=null){humii.innerHTML=response.feeds[4].field2 }
+    else{humii22.innerHTML=humii22.innerHTML}
+
+
+    if(response.feeds[5].field2 !=null){humii.innerHTML=response.feeds[5].field2 }
+    else{humii22.innerHTML=humii22.innerHTML}
+
+
+ if(response.feeds[6].field2  !=null){humii.innerHTML=response.feeds[6].field2 }
+ else{humii22.innerHTML=humii22.innerHTML}
+
+
+   if(response.feeds[7].field2  !=null){humii.innerHTML=response.feeds[7].field2 }
+   else{humii22.innerHTML=humii22.innerHTML}
+
+
+  if(response.feeds[8].field2  !=null){humii.innerHTML=response.feeds[8].field2 }
+  else{humii22.innerHTML=humii22.innerHTML}
+
+
+  if(response.feeds[9].field2  !=null){humii.innerHTML=response.feeds[9].field2 }
+  else{humii22.innerHTML=humii22.innerHTML}
+
+  
+  if(response.feeds[10].field2 !=null){humii.innerHTML=response.feeds[10].field2 }
+  else{humii22.innerHTML=humii22.innerHTML}
+
+
+if(response.feeds[11].field2  !=null){humii.innerHTML=response.feeds[11].field2 }
+else{humii22.innerHTML=humii22.innerHTML}
+
+
+ if(response.feeds[12].field2  !=null){humii.innerHTML=response.feeds[12].field2 }
+ else{humii22.innerHTML=humii22.innerHTML}
+
+
+if(response.feeds[13].field2  !=null){humii.innerHTML=response.feeds[13].field2 }
+else{humii22.innerHTML=humii22.innerHTML}
+
+
+if(response.feeds[14].field2  !=null){humii.innerHTML=response.feeds[14].field2 }
+else{humii22.innerHTML=humii22.innerHTML}
+
+
+
+
+
+
+  if(response.feeds[0].field3  !=null){rainy.innerHTML=response.feeds[0].field3 }
+    else{raain.innerHTML=raain.innerHTML}
+ 
+
+   if(response.feeds[1].field3  !=null){rainy.innerHTML=response.feeds[1].field3 }
+   else{raain.innerHTML=raain.innerHTML}
+
+
+
+    if(response.feeds[2].field3 !=null){rainy.innerHTML=response.feeds[2].field3}
+    else{raain.innerHTML=raain.innerHTML}
+
+  if(response.feeds[3].field3  !=null){rainy.innerHTML=response.feeds[3].field3 }
+ else{raain.innerHTML=raain.innerHTML}
+
+    if(response.feeds[4].field3  !=null){rainy.innerHTML=response.feeds[4].field3 }
+    else{raain.innerHTML=raain.innerHTML}
+
+
+    if(response.feeds[5].field3 !=null){rainy.innerHTML=response.feeds[5].field3 }
+    else{raain.innerHTML=raain.innerHTML}
+
+
+ if(response.feeds[6].field3  !=null){rainy.innerHTML=response.feeds[6].field3 }
+ else{raain.innerHTML=raain.innerHTML}
+
+
+   if(response.feeds[7].field3  !=null){rainy.innerHTML=response.feeds[7].field3 }
+   else{raain.innerHTML=raain.innerHTML}
+
+
+  if(response.feeds[8].field3  !=null){rainy.innerHTML=response.feeds[8].field3 }
+  else{raain.innerHTML=raain.innerHTML}
+
+
+  if(response.feeds[9].field3  !=null){rainy.innerHTML=response.feeds[9].field3 }
+  else{raain.innerHTML=raain.innerHTML}
+
+  if(response.feeds[10].field3 !=null){rainy.innerHTML=response.feeds[10].field3 }
+  else{raain.innerHTML=raain.innerHTML}
+
+
+if(response.feeds[11].field3  !=null){rainy.innerHTML=response.feeds[11].field3 }
+else{raain.innerHTML=raain.innerHTML}
+
+
+ if(response.feeds[12].field3  !=null){rainy.innerHTML=response.feeds[12].field3 }
+ else{raain.innerHTML=raain.innerHTML}
+
+
+if(response.feeds[13].field3  !=null){rainy.innerHTML=response.feeds[13].field3 }
+else{raain.innerHTML=raain.innerHTML}
+
+
+if(response.feeds[14].field3  !=null){rainy.innerHTML=response.feeds[14].field3 }
+else{raain.innerHTML=raain.innerHTML}
+
+
+
+
+
+  
+  if(response.feeds[0].field4  !=null){soili.innerHTML=response.feeds[0].field4 }
+    else{sooil.innerHTML=sooil.innerHTML}
+ 
+
+   if(response.feeds[1].field4  !=null){soili.innerHTML=response.feeds[1].field4 }
+   else{sooil.innerHTML=sooil.innerHTML}
+
+
+
+    if(response.feeds[2].field4 !=null){soili.innerHTML=response.feeds[2].field4}
+    else{sooil.innerHTML=sooil.innerHTML}
+
+  if(response.feeds[3].field4  !=null){soili.innerHTML=response.feeds[3].field4 }
+ else{sooil.innerHTML=sooil.innerHTML}
+
+    if(response.feeds[4].field4  !=null){soili.innerHTML=response.feeds[4].field4 }
+    else{sooil.innerHTML=sooil.innerHTML}
+
+
+    if(response.feeds[5].field4 !=null){soili.innerHTML=response.feeds[5].field4 }
+    else{sooil.innerHTML=sooil.innerHTML}
+
+
+ if(response.feeds[6].field4  !=null){soili.innerHTML=response.feeds[6].field4 }
+ else{sooil.innerHTML=sooil.innerHTML}
+
+
+   if(response.feeds[7].field4  !=null){soili.innerHTML=response.feeds[7].field4 }
+   else{sooil.innerHTML=sooil.innerHTML}
+
+
+  if(response.feeds[8].field4  !=null){soili.innerHTML=response.feeds[8].field4 }
+  else{sooil.innerHTML=sooil.innerHTML}
+
+
+  if(response.feeds[9].field4  !=null){soili.innerHTML=response.feeds[9].field4 }
+  else{sooil.innerHTML=sooil.innerHTML}
+
+  if(response.feeds[10].field4 !=null){soili.innerHTML=response.feeds[10].field4 }
+  else{sooil.innerHTML=sooil.innerHTML}
+
+
+if(response.feeds[11].field4  !=null){soili.innerHTML=response.feeds[11].field4 }
+else{sooil.innerHTML=sooil.innerHTML}
+
+
+ if(response.feeds[12].field4  !=null){soili.innerHTML=response.feeds[12].field4 }
+ else{sooil.innerHTML=sooil.innerHTML}
+
+
+if(response.feeds[13].field4  !=null){soili.innerHTML=response.feeds[13].field4 }
+else{sooil.innerHTML=sooil.innerHTML}
+
+
+if(response.feeds[14].field4  !=null){soili.innerHTML=response.feeds[14].field4 }
+else{sooil.innerHTML=sooil.innerHTML}
+ 
+
+
+
+
+  if(response.feeds[0].field5  !=null){pressurii.innerHTML=response.feeds[0].field5 }
+    else{prepre.innerHTML=prepre.innerHTML}
+ 
+
+   if(response.feeds[1].field5  !=null){pressurii.innerHTML=response.feeds[1].field5 }
+   else{prepre.innerHTML=prepre.innerHTML}
+
+
+
+    if(response.feeds[2].field5 !=null){pressurii.innerHTML=response.feeds[2].field5}
+    else{prepre.innerHTML=prepre.innerHTML}
+
+  if(response.feeds[3].field5  !=null){pressurii.innerHTML=response.feeds[3].field5 }
+ else{prepre.innerHTML=prepre.innerHTML}
+
+    if(response.feeds[4].field5  !=null){pressurii.innerHTML=response.feeds[4].field5 }
+    else{prepre.innerHTML=prepre.innerHTML}
+
+
+    if(response.feeds[5].field5 !=null){pressurii.innerHTML=response.feeds[5].field5 }
+    else{prepre.innerHTML=prepre.innerHTML}
+
+
+ if(response.feeds[6].field5  !=null){pressurii.innerHTML=response.feeds[6].field5 }
+ else{prepre.innerHTML=prepre.innerHTML}
+
+
+   if(response.feeds[7].field5  !=null){pressurii.innerHTML=response.feeds[7].field5 }
+   else{prepre.innerHTML=prepre.innerHTML}
+
+
+  if(response.feeds[8].field5  !=null){pressurii.innerHTML=response.feeds[8].field5 }
+  else{prepre.innerHTML=prepre.innerHTML}
+
+
+  if(response.feeds[9].field5  !=null){pressurii.innerHTML=response.feeds[9].field5 }
+  else{prepre.innerHTML=prepre.innerHTML}
+
+  if(response.feeds[10].field5 !=null){pressurii.innerHTML=response.feeds[10].field5 }
+  else{prepre.innerHTML=prepre.innerHTML}
+
+
+if(response.feeds[11].field5  !=null){pressurii.innerHTML=response.feeds[11].field5 }
+else{prepre.innerHTML=prepre.innerHTML}
+
+
+ if(response.feeds[12].field5  !=null){pressurii.innerHTML=response.feeds[12].field5 }
+ else{prepre.innerHTML=prepre.innerHTML}
+
+
+if(response.feeds[13].field5  !=null){pressurii.innerHTML=response.feeds[13].field5 }
+else{prepre.innerHTML=prepre.innerHTML}
+
+
+if(response.feeds[14].field5  !=null){pressurii.innerHTML=response.feeds[14].field5 }
+else{prepre.innerHTML=prepre.innerHTML}
+
+
+
+
+
+
+
+  
+  if(response.feeds[0].field6  !=null){heatii.innerHTML=response.feeds[0].field6 }
+    else{height.innerHTML=height.innerHTML}
+ 
+
+   if(response.feeds[1].field6  !=null){heatii.innerHTML=response.feeds[1].field6 }
+   else{height.innerHTML=height.innerHTML}
+
+
+
+    if(response.feeds[2].field6 !=null){heatii.innerHTML=response.feeds[2].field6}
+    else{height.innerHTML=height.innerHTML}
+
+  if(response.feeds[3].field6  !=null){heatii.innerHTML=response.feeds[3].field6 }
+ else{height.innerHTML=height.innerHTML}
+
+    if(response.feeds[4].field6  !=null){heatii.innerHTML=response.feeds[4].field6 }
+    else{height.innerHTML=height.innerHTML}
+
+
+    if(response.feeds[5].field6 !=null){heatii.innerHTML=response.feeds[5].field6 }
+    else{height.innerHTML=height.innerHTML}
+
+
+ if(response.feeds[6].field6  !=null){heatii.innerHTML=response.feeds[6].field6 }
+ else{height.innerHTML=height.innerHTML}
+
+
+   if(response.feeds[7].field6  !=null){heatii.innerHTML=response.feeds[7].field6 }
+   else{height.innerHTML=height.innerHTML}
+
+
+  if(response.feeds[8].field6  !=null){heatii.innerHTML=response.feeds[8].field6 }
+  else{height.innerHTML=height.innerHTML}
+
+
+  if(response.feeds[9].field6  !=null){heatii.innerHTML=response.feeds[9].field6 }
+  else{height.innerHTML=height.innerHTML}
+
+  if(response.feeds[10].field6 !=null){heatii.innerHTML=response.feeds[10].field6 }
+  else{height.innerHTML=height.innerHTML}
+
+
+if(response.feeds[11].field6  !=null){heatii.innerHTML=response.feeds[11].field6 }
+else{height.innerHTML=height.innerHTML}
+
+
+ if(response.feeds[12].field6  !=null){heatii.innerHTML=response.feeds[12].field6 }
+ else{height.innerHTML=height.innerHTML}
+
+
+if(response.feeds[13].field6  !=null){heatii.innerHTML=response.feeds[13].field6 }
+else{height.innerHTML=height.innerHTML}
+
+
+if(response.feeds[14].field6  !=null){heatii.innerHTML=response.feeds[14].field6 }
+else{height.innerHTML=height.innerHTML}
+
+
+
+
+
+
+
+  if(response.feeds[0].field7  !=null){vissy.innerHTML=response.feeds[0].field7 }
+  else{cisco.innerHTML=cisco.innerHTML}
+ 
+
+   if(response.feeds[1].field7  !=null){vissy.innerHTML=response.feeds[1].field6 }
+   else{cisco.innerHTML=cisco.innerHTML}
+
+
+
+    if(response.feeds[2].field7 !=null){vissy.innerHTML=response.feeds[2].field7}
+    else{cisco.innerHTML=cisco.innerHTML}
+
+  if(response.feeds[3].field7  !=null){vissy.innerHTML=response.feeds[3].field7 }
+ else{cisco.innerHTML=cisco.innerHTML}
+
+    if(response.feeds[4].field7  !=null){vissy.innerHTML=response.feeds[4].field7 }
+    else{cisco.innerHTML=cisco.innerHTML}
+
+
+    if(response.feeds[5].field7 !=null){vissy.innerHTML=response.feeds[5].field7 }
+    else{cisco.innerHTML=cisco.innerHTML}
+
+
+ if(response.feeds[6].field7  !=null){vissy.innerHTML=response.feeds[6].field7 }
+ else{cisco.innerHTML=cisco.innerHTML}
+
+
+   if(response.feeds[7].field7  !=null){vissy.innerHTML=response.feeds[7].field7 }
+   else{cisco.innerHTML=cisco.innerHTML}
+
+
+  if(response.feeds[8].field7  !=null){vissy.innerHTML=response.feeds[8].field7 }
+  else{cisco.innerHTML=cisco.innerHTML}
+
+
+  if(response.feeds[9].field7  !=null){vissy.innerHTML=response.feeds[9].field7 }
+  else{cisco.innerHTML=cisco.innerHTML}
+
+  if(response.feeds[10].field7 !=null){vissy.innerHTML=response.feeds[10].field7 }
+  else{cisco.innerHTML=cisco.innerHTML}
+
+
+if(response.feeds[11].field7  !=null){vissy.innerHTML=response.feeds[11].field7 }
+else{cisco.innerHTML=cisco.innerHTML}
+
+
+ if(response.feeds[12].field7  !=null){vissy.innerHTML=response.feeds[12].field7 }
+ else{cisco.innerHTML=cisco.innerHTML}
+
+
+if(response.feeds[13].field7  !=null){vissy.innerHTML=response.feeds[13].field7 }
+else{cisco.innerHTML=cisco.innerHTML}
+
+
+if(response.feeds[14].field7  !=null){vissy.innerHTML=response.feeds[14].field7 }
+else{cisco.innerHTML=cisco.innerHTML}
+
+
+
+
+
+
+
+
+  
+})
+.catch(err => console.error(err));
